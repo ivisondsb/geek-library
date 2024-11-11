debf7c58084ddef8e0f4624ceebebb778e82b68a
ivisondsb <ivisonk0@gmail.com>
2024-11-11 16:10:55 -0300

    first commit

---
+++ src/main/kotlin/br/com/ivisondsb/geek_library/App.kt
@@ -0,0 +1,11 @@
+package br.com.ivisondsb.geek_library
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class App
+
+fun main(args: Array<String>) {
+    runApplication<App>(*args)
+}
